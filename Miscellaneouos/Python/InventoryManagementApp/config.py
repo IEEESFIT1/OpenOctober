8bbfb195c714ce868ee52203c05ba7aa37b1c8f1
imakshat47 <imakshat@github.com>
2020-10-20 16:42:03 +0530

    Python Inventory Management App

---
+++ Miscellaneouos/Python/InventoryManagementApp/config.py
@@ -0,0 +1,62 @@
+# by: Imakshat47
+
+## Config File for System
+
+# System basic details
+app_name = "Warehouse Inventory Sales Purchase Management System (WISPMS) "
+title = "Warehouse Inventory Sales Purchase"
+window_size = "1500x690"
+
+# DB Config Details
+db_name = "inventory.db"
+table_name = "product"
+
+# Element Size
+win_width = 1500
+bodyWidth = 1500
+bodyHeight = 500
+bodyChildHeight = 380
+bodyChildWidthL = 600
+bodyChildWidthR = 400
+inputWidth = 30
+
+
+# padding
+padding10 = 10
+padding20 = 20
+padding30 = 30
+padding50 = 50
+
+# Button Config
+btn_height = 1
+btn_width = 12
+btn_primary = "#1d2d50"
+btn_secondary = "#ffffff"
+
+# Color Configs
+titleColor = "#1d2d50"
+fontColor = "#ffffff"
+
+listColor = "#1e5f74"
+
+inputColor = "#1e5f74"
+inputFontColor = "#ffffff"
+
+primary_bg = "#1e5f74"
+secondary_bg = "#ffffff"
+
+frontPrimary_bg = "#ffffff"
+
+whiteColor = '#ffffff'
+
+# Border
+borderSmall = 0
+borderLight = 4
+
+# Font Config
+fontFamily = "arial"
+fontWeight = "bold"
+tinyFont = 14
+smallFont = 18
+mediumFont = 28
+largeFont = 50
