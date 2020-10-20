ab7304a30aadd3810d076279382783e4885e7e97
Pranav-yadav <pranavyadav3912@gmail.com>
2020-10-20 17:37:30 +0530

    Added Postfix Evaluation Program

--- SoftwareDev/C/InfixToPostfix.c
+++ SoftwareDev/C/InfixToPostfix.c
@@ -83,7 +83,7 @@ int getPriority(char op)
 {
     if(op=='/' || op == '*' || op=='%')
         return 1;
-    else if(op=='+' || op=='–')
+    else if(op=='+' || op=='ï¿½')
         return 0;
 }
 void push(char st[], char val)
