61cd78f5ac1087d4aa162faf41d049aa4b5bbc37
saketh bachu <saketh7000@gmail.com>
2020-10-29 21:50:44 +0530

    Added VGG16

---
+++ ML_AI/Python/PyTorch/vgg16_model.py
@@ -0,0 +1,286 @@
+import time
+# import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+
+if torch.cuda.is_available():
+    torch.backends.cudnn.deterministic = True
+
+path = 'path/to/the/master'
+
+# Device
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('Device:', DEVICE)
+
+# hyperparameter
+# random_seed = 1
+learning_rate = 0.00005
+num_epochs = 50
+batch_size = 10
+
+num_classes = 4
+IMG_SIZE = (32, 32)   # resize image
+# IMG_MEAN = [0.485, 0.456, 0.406]
+# IMG_STD = [0.229, 0.224, 0.225]
+
+transforms = transforms.Compose({
+    transforms.Resize(IMG_SIZE),
+    transforms.ToTensor()
+    # transforms.Normalize(IMG_MEAN, IMG_STD)
+})
+
+
+class MyDataset(torch.utils.data.Dataset):
+    def __init__(self, root, datatxt, transform=None, target_transform=None):
+        super(MyDataset, self).__init__()
+        with open(root + datatxt, 'r') as f:
+            imgs = []
+            for line in f:
+                line = line.rstrip()
+                words = line.split()
+                imgs.append((words[0], int(words[1])))
+            self.root = root
+            self.imgs = imgs
+            self.transform = transform
+            self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        f, label = self.imgs[index]  
+        img = Image.open(self.root + f).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, label
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+train_data = MyDataset(path + 'train_data/', 'train.txt', transform=transforms)
+test_data = MyDataset(path + 'test_data/', 'test.txt', transform=transforms)
+
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=batch_size)
+
+"""
+vgg16
+"""
+class VGG16(nn.Module):
+
+    def __init__(self, num_classes):
+        super(VGG16, self).__init__()
+
+        # calculate same padding:
+        # (w - k + 2*p)/s + 1 = o
+        # => p = (s(o-1) - w + k)/2
+
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=3,
+                      out_channels=64,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      # (1(32-1)- 32 + 3)/2 = 1
+                      padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64,
+                      out_channels=64,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2),
+                         stride=(2, 2))
+        )
+
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=64,
+                      out_channels=128,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128,
+                      out_channels=128,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2),
+                         stride=(2, 2))
+        )
+        
+        self.block_3 = nn.Sequential(
+            nn.Conv2d(in_channels=128,
+                      out_channels=256,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256,
+                      out_channels=256,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256,
+                      out_channels=256,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2),
+                         stride=(2, 2))
+        )
+
+        self.block_4 = nn.Sequential(
+            nn.Conv2d(in_channels=256,
+                      out_channels=512,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2),
+                         stride=(2, 2))
+        )
+
+        self.block_5 = nn.Sequential(
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512,
+                      out_channels=512,
+                      kernel_size=(3, 3),
+                      stride=(1, 1),
+                      padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2),
+                         stride=(2, 2))
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.65),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(p=0.65),
+            nn.Linear(4096, num_classes),
+        )
+
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+                nn.init.kaiming_uniform_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+#                 nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    m.bias.detach().zero_()
+
+        # self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+
+    def forward(self, x):
+
+        x = self.block_1(x)
+        x = self.block_2(x)
+        x = self.block_3(x)
+        x = self.block_4(x)
+        x = self.block_5(x)
+        # x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        logits = self.classifier(x)
+        probas = F.softmax(logits, dim=1)
+        # probas = nn.Softmax(logits)
+        return probas
+        # return logits
+
+
+# torch.manual_seed(random_seed)
+model = VGG16(num_classes=num_classes)
+model = model.to(DEVICE)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+"""
+train
+"""
+for epoch in range(num_epochs):
+    start = time.perf_counter()
+    # model.train()
+    running_loss = 0.0
+    correct_pred = 0
+    for index, data in enumerate(train_loader):
+        image, label = data
+        image = image.to(DEVICE)
+        label = label.to(DEVICE)
+        y_pred = model(image)
+        
+        _, pred = torch.max(y_pred, 1)
+        correct_pred += (pred == label).sum()
+        
+        loss = criterion(y_pred, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+                
+        running_loss += float(loss.item())
+    end = time.perf_counter()
+    print('epoch {}/{}\tTrain loss: {:.4f}\tTrain accuracy: {:.2f}%'.
+          format(epoch + 1, num_epochs, running_loss / (index + 1), correct_pred.item() / (batch_size * (index + 1)) * 100))
+    print('Time: {:.2f}s'.format(end - start))
+print('Finished training!')
+
+"""
+test
+"""
+test_loss = 0.0
+correct_pred = 0
+model.eval()
+for _, data in enumerate(test_loader):
+    image, label = data
+    image = image.to(DEVICE)
+    lable = label.to(DEVICE)
+    y_pred = model(image)
+    
+    _, pred = torch.max(y_pred, 1)
+    correct_pred += (pred == label).sum()
+    
+    loss = criterion(y_pred, label)
+    test_loss += float(loss.item())
+print('Test loss: {:.4f}\tTest accuracy: {:.2f}%'.format(test_loss / 12, correct_pred.item() / 120 * 100))
