8c173697536c28f41f212f0233bdd284a73b7884
mssahumanisha <67999137+mssahumanisha@users.noreply.github.com>
2020-10-30 07:39:12 +0530

    solved issues

--- SoftwareDev/Python/stringxor.py
+++ SoftwareDev/Python/stringxor.py
@@ -1,10 +1,10 @@
 def strings_xor(s, t):
     res = ""
-    for i in range(len(s)):
+    for i,t in enumerate(s):
         if s[i] == t[i]:
-            res += '0';
+            res += '0'
         else:
-            res += '1';
+            res += '1'
     return res
 s = input()
 t = input()
