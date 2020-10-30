463d43389a46c756e58121218070bf76197c9d4e
CassandraRodrigues <cassyrods23@gmail.com>
2020-10-30 19:50:48 +0530

    Added Codes for Encryption and Decryption

--- SoftwareDev/C/Playfairdecryption.c
+++ SoftwareDev/C/Playfairdecryption.c
@@ -1,10 +1,10 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h> 
-  
 #define SIZE 30 
   
-// Function to convert the string to lowercase 
+// Convert all the characters 
+// of a string to lowercase 
 void toLowerCase(char plain[], int ps) 
 { 
     int i; 
@@ -14,7 +14,8 @@ void toLowerCase(char plain[], int ps)
     } 
 } 
   
-// Function to remove all spaces in a string 
+// Remove all spaces in a string 
+// can be extended to remove punctuation 
 int removeSpaces(char* plain, int ps) 
 { 
     int i, count = 0; 
@@ -25,24 +26,24 @@ int removeSpaces(char* plain, int ps)
     return count; 
 } 
   
-// Function to generate the 5x5 key square 
-void generateKeyTable(char key[], int ks, char keyT[5][5]) 
+// generates the 5x5 key square 
+void generateKeyTable(char key[], int ks, 
+                      char keyT[5][5]) 
 { 
     int i, j, k, flag = 0, *dicty; 
   
     // a 26 character hashmap 
     // to store count of the alphabet 
     dicty = (int*)calloc(26, sizeof(int)); 
+  
     for (i = 0; i < ks; i++) { 
         if (key[i] != 'j') 
             dicty[key[i] - 97] = 2; 
     } 
-  
     dicty['j' - 97] = 1; 
   
     i = 0; 
     j = 0; 
-  
     for (k = 0; k < ks; k++) { 
         if (dicty[key[k] - 97] == 2) { 
             dicty[key[k] - 97] -= 1; 
@@ -54,7 +55,6 @@ void generateKeyTable(char key[], int ks, char keyT[5][5])
             } 
         } 
     } 
-  
     for (k = 0; k < 26; k++) { 
         if (dicty[k] == 0) { 
             keyT[i][j] = (char)(k + 97); 
@@ -67,9 +67,10 @@ void generateKeyTable(char key[], int ks, char keyT[5][5])
     } 
 } 
   
-// Function to search for the characters of a digraph 
+// Search for the characters of a digraph 
 // in the key square and return their position 
-void search(char keyT[5][5], char a, char b, int arr[]) 
+void search(char keyT[5][5], char a, 
+            char b, int arr[]) 
 { 
     int i, j; 
   
@@ -79,9 +80,7 @@ void search(char keyT[5][5], char a, char b, int arr[])
         b = 'i'; 
   
     for (i = 0; i < 5; i++) { 
-  
         for (j = 0; j < 5; j++) { 
-  
             if (keyT[i][j] == a) { 
                 arr[0] = i; 
                 arr[1] = j; 
@@ -100,32 +99,19 @@ int mod5(int a)
     return (a % 5); 
 } 
   
-// Function to make the plain text length to be even 
-int prepare(char str[], int ptrs) 
-{ 
-    if (ptrs % 2 != 0) { 
-        str[ptrs++] = 'z'; 
-        str[ptrs] = '\0'; 
-    } 
-    return ptrs; 
-} 
-  
-// Function for performing the encryption 
-void encrypt(char str[], char keyT[5][5], int ps) 
+// Function to decrypt 
+void decrypt(char str[], char keyT[5][5], int ps) 
 { 
     int i, a[4]; 
-  
     for (i = 0; i < ps; i += 2) { 
-  
         search(keyT, str[i], str[i + 1], a); 
-  
         if (a[0] == a[2]) { 
-            str[i] = keyT[a[0]][mod5(a[1] + 1)]; 
-            str[i + 1] = keyT[a[0]][mod5(a[3] + 1)]; 
+            str[i] = keyT[a[0]][mod5(a[1] - 1)]; 
+            str[i + 1] = keyT[a[0]][mod5(a[3] - 1)]; 
         } 
         else if (a[1] == a[3]) { 
-            str[i] = keyT[mod5(a[0] + 1)][a[1]]; 
-            str[i + 1] = keyT[mod5(a[2] + 1)][a[1]]; 
+            str[i] = keyT[mod5(a[0] - 1)][a[1]]; 
+            str[i + 1] = keyT[mod5(a[2] - 1)][a[1]]; 
         } 
         else { 
             str[i] = keyT[a[0]][a[3]]; 
@@ -134,8 +120,8 @@ void encrypt(char str[], char keyT[5][5], int ps)
     } 
 } 
   
-// Function to encrypt using Playfair Cipher 
-void encryptByPlayfairCipher(char str[], char key[]) 
+// Function to call decrypt 
+void decryptByPlayfairCipher(char str[], char key[]) 
 { 
     char ps, ks, keyT[5][5]; 
   
@@ -144,16 +130,14 @@ void encryptByPlayfairCipher(char str[], char key[])
     ks = removeSpaces(key, ks); 
     toLowerCase(key, ks); 
   
-    // Plaintext 
+    // ciphertext 
     ps = strlen(str); 
     toLowerCase(str, ps); 
     ps = removeSpaces(str, ps); 
   
-    ps = prepare(str, ps); 
-  
     generateKeyTable(key, ks, keyT); 
   
-    encrypt(str, keyT, ps); 
+    decrypt(str, keyT, ps); 
 } 
   
 // Driver code 
@@ -165,14 +149,14 @@ int main()
     strcpy(key, "Monarchy"); 
     printf("Key text: %s\n", key); 
   
-    // Plaintext to be encrypted 
-    strcpy(str, "instruments"); 
+    // Ciphertext to be decrypted 
+    strcpy(str, "gatlmzclrqtx"); 
     printf("Plain text: %s\n", str); 
   
     // encrypt using Playfair Cipher 
-    encryptByPlayfairCipher(str, key); 
+    decryptByPlayfairCipher(str, key); 
   
-    printf("Cipher text: %s\n", str); 
+    printf("Deciphered text: %s\n", str); 
   
     return 0; 
 } 
