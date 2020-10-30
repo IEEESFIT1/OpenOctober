faabf5e1afa4e415627c87cce665302fa4bca768
Rakshita <28rakshi@gmail.com>
2020-10-30 18:06:10 +0530

    Add pigeonholeSort.py

--- SoftwareDev/Python/pigeonholeSort.py
+++ SoftwareDev/Python/pigeonholeSort.py
@@ -1,20 +1,25 @@
 # Implementation of the pigeonhole sort using python
-def pigeonhole_sort(a): 
-	my_min = min(a) 
-	my_max = max(a) 
-	size = my_max - my_min + 1 
-	holes = [0] * size 
-	for x in a: 
-		assert type(x) is int, "integers only please"
-		holes[x - my_min] += 1
-	i = 0
-	for count in range(size): 
-		while holes[count] > 0: 
-			holes[count] -= 1
-			a[i] = count + my_min 
-			i += 1
-a = [8, 3, 2, 7, 4, 6, 8] 
-print("Sorted order is : ", end =" ") 
-pigeonhole_sort(a) 
-for i in range(0, len(a)): 
-	print(a[i], end =" ") 
+def pigeonhole_sort(a):
+    >>> a = [8, 3, 2, 7, 4, 6, 8]
+    >>> b = sorted(a)  
+    >>> pigeonhole_sort(a)  
+    >>> a == b
+    True
+    min_val = min(a)  
+    max_val = max(a)  
+    size = max_val - min_val + 1  
+    holes = [0] * size
+    for x in a:
+        assert isinstance(x, int), "integers only please"
+        holes[x - min_val] += 1
+    i = 0
+    for count in range(size):
+        while holes[count] > 0:
+            holes[count] -= 1
+            a[i] = count + min_val
+            i += 1
+def main():
+    pigeonhole_sort([8, 3, 2, 7, 4, 6, 8])
+    print("Sorted order is: ", " ", join(a))
+if __name__ == "__main__":
+    main()
