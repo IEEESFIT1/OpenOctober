42ba9b920f951cb1552280520e2b1a49a3ac56c0
Almas <62244354+almasfiza@users.noreply.github.com>
2021-10-11 15:37:41 +0530

    Update threeSumClosest.py

--- Data Structures/Arrays/threeSumClosest.py
+++ Data Structures/Arrays/threeSumClosest.py
@@ -2,7 +2,7 @@ class threeSumClosest:
     def threeSumClosestFn(self, nums: List[int], target: int) -> int:
       #just a random high value
         best_sum = 100000
-        sum = 0
+        
         nums.sort()
         for i in range(0,len(nums)-2):
             if nums[i] == nums[i-1] and i>0:
