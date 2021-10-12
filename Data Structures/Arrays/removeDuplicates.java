bd132e39a4377f582b9c50ee1a5bad66345fb8b0
Almas <62244354+almasfiza@users.noreply.github.com>
2021-10-12 16:19:40 +0530

    Update removeDuplicates.java

--- Data Structures/Arrays/removeDuplicates.java
+++ Data Structures/Arrays/removeDuplicates.java
@@ -1,5 +1,5 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
+class removeDuplicates {
+    public int removeDuplicatesFn(int[] nums) {
         if (nums.length == 0) return 0;
     int i = 0;
     for (int j = 1; j < nums.length; j++) {
